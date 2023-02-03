5bb8f8dec60391f320d68b2c1222cb4d68752916
Andrew Njoo <adnjoo@gmail.com>
2023-02-03 12:28:09 -0800

    feat: add persistent data

--- src/store/eventsStore.jsx
+++ src/store/eventsStore.jsx
@@ -1,16 +1,20 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
-export const useEventsStore = create((set) => ({
-  events: [
-    { title: 'event 1', date: '2023-02-01' },
-    { title: 'event 2', date: '2023-02-02' },
-  ],
-
-  addEvent: (event) => set((state) => ({
-    events: [...state.events, event],
-  })),
-
-  deleteEvent: (event) => set((state) => ({
-    events: state.events.filter((e) => e.title !== event.title),
-  })),
-}));
+export const useEventsStore = create(
+  persist(
+    (set) => ({
+      events: [],
+      addEvent: (event) => set((state) => ({
+        events: [...state.events, event],
+      })),
+      deleteEvent: (event) => set((state) => ({
+        events: state.events.filter((e) => e.title !== event.title),
+      })),
+    }),
+    {
+      name: 'events-storage', // name of the item in the storage (must be unique)
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
