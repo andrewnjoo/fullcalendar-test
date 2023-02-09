d85a92ed8db42c38fa7180405ead2a9af9f2fd96
Andrew Njoo <adnjoo@gmail.com>
2023-02-08 21:18:51 -0800

    feat: add export data as JSON, load data

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,32 @@ function App() {
     setOpenAddEventModal(true);
   };
 
+  const handleExport = () => {
+    const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(events))}`;
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute('href', dataStr);
+    downloadAnchorNode.setAttribute('download', 'events.json');
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  };
+
+  const handleLoad = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'application/json';
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsText(file, 'UTF-8');
+      reader.onload = (readerEvent) => {
+        const content = readerEvent.target.result;
+        useEventsStore.setState({ events: JSON.parse(content) });
+      };
+    };
+    input.click();
+  };
+
   return (
     <div className="my-6 mx-2 sm:mx-12">
       <div className="flex justify-center text-3xl font-bold"> Minimal Journal</div>
@@ -49,6 +75,20 @@ function App() {
           e.el.style.cursor = 'pointer';
         }}
       />
+      <div className="flex flex-row justify-center gap-10">
+        <button
+          className="my-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleExport}
+        >
+          Export data as JSON
+        </button>
+        <button
+          className="my-6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleLoad}
+        >
+          Load data
+        </button>
+      </div>
     </div>
   );
 }
